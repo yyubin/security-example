0706889143e51c8644b44b8ef8644dad69bfdafc
yyubin <hazing120@gmail.com>
2023-01-17 10:32:29 +0900

    Update UserDetails error

--- src/main/java/practice/security/service/MemberService.java
+++ src/main/java/practice/security/service/MemberService.java
@@ -1,14 +1,22 @@
 package practice.security.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import practice.security.domain.Member;
 import practice.security.dto.MemberDto;
 import practice.security.repository.MemberRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -27,9 +35,17 @@ public class MemberService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Member> memberWrapper = memberRepository.findByUsername(username);
+        Member member = memberWrapper.get();
 
+        List<GrantedAuthority> authorities = new ArrayList<>();
 
+        if("admin".equals(username)){
+            authorities.add(new SimpleGrantedAuthority(Role.ADMIN.getValue()));
+        }else {
+            authorities.add(new SimpleGrantedAuthority(Role.MEMBER.getValue()));
+        }
 
-        return null;
+        return new User(member.getUsername(), member.getPassword(), authorities);
     }
 }
